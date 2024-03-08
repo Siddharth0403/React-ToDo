9fbba8850ea6be80bd33a3660de05e03b19193db
Siddharth Vaidya <siddharthvaidya18@gmail.com>
2024-03-08 19:39:14 +0530

    First Commit

---
+++ src/Components/Background.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Background() {
+  return (
+    <>
+      <div className="bg">
+        <h1
+          className="
+      bgText"
+        >
+          Docs
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default Background;
