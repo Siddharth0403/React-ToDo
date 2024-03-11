0cc14bbd8e698e04a48eac9723c451a23f788e69
Siddharth Vaidya <siddharthvaidya18@gmail.com>
2024-03-11 11:00:00 +0530

    11 FEB

--- src/Components/Background.jsx
+++ src/Components/Background.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Note from "./Note";
 
 function Background() {
   return (
@@ -10,6 +11,7 @@ function Background() {
         >
           Docs
         </h1>
+        <Note />
       </div>
     </>
   );
---
+++ src/Components/Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="mainNote">
+      <div className="head">
+        <h5 className="icon">icon</h5>
+        <h5>Note</h5>
+      </div>
+      <div className="footer"></div>
+    </div>
+  );
+}
+
+export default Note;
